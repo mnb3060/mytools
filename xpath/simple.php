8dc261322e24d7c7e0ae4bd392404bf86a5d9239
mnb3060 <90744038+mnb3060@users.noreply.github.com>
2024-03-29 21:48:29 +0330

    Update simple.php

--- xpath/simple.php
+++ xpath/simple.php
@@ -1,5 +1,13 @@
 <?php
+
+
 $contents = file_get_contents('https://yoursite.com/');
+
+function getval($xpath , $query){
+	//return $r = trim((string) $xpath->query($query)->item(0)->nodeValue);
+	return $r = $xpath->query($query)->item(0)->nodeValue;	
+}
+
 $dom = new DOMDocument();
 @$dom->loadHTML($contents);
 $xpath = new DOMXpath($dom);	
