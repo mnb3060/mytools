60079c814895a10e9f01e98f0b4800ae2b8494f1
mnb3060 <90744038+mnb3060@users.noreply.github.com>
2024-05-11 10:31:56 +0330

    Update install-chisel.sh

--- install-chisel.sh
+++ install-chisel.sh
@@ -1,20 +1,19 @@
-#!/bin/sh
-
-echo "install chisel from the github rep"
-mkdir chisel && cd chisel
-
-echo "Downlod chisel for linux amd64"
-wget https://github.com/jpillora/chisel/releases/download/v1.9.1/chisel_1.9.1_linux_amd64.gz && gunzip chisel_1.9.1_linux_amd64.gz && chmod +x chisel_1.9.1_linux_amd64
+#!/bin/bash
 
 echo "setup chisel for server or client? type s for server and c for client"
 read chtype
+echo "type your address for server or client to connect: [recommended domain with cloudflare proxy enabled]"
+read addr
+echo "type your port for chisel:[recommended usually port , for example 80,443,8080,2082,2083,10000 etc...]"
+read port
 
-if[$chtype == 's']
-  then
-  echo 'server selected'
-elif[$chtype == 'c']
-  then
-  echo 'client selected'
+echo "Are you want to run the service autorun after booutup? [recommend yes!] Y/N?"
+read service
+
+if [[ "$chtype" == "s" ]]; then
+  echo "server selected"
+elif [[ "$chtype" == "c" ]]; then
+  echo "client selected"
 else
-  echo 'not selected!!'  
+  echo "not selected!!"
 fi
